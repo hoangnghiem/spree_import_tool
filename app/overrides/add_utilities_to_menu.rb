fb54a42c60d31547826fbe51996c0642ec133a9e
hoangnghiem <hoangnghiem1711@gmail.com>
2015-11-10 10:12:27 +0700

    Add original deface

--- app/overrides/add_utilities_to_menu.rb
+++ app/overrides/add_utilities_to_menu.rb
@@ -2,4 +2,5 @@ Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "utilities_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
                      :partial => "spree/admin/utilities/menu",
+                     :original => '182af4adb60a96edfb5b6e7c40b4c1c0dcf2d314',
                      :disabled => false)
